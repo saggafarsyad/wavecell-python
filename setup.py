f3e6dca2c02827e5ead3f49026f81802517961cd
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-04-21 21:33:37 +0700

    chore: added metadata for pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wavecell-py",
+    version="0.0.1",
+    author="Saggaf Arsyad",
+    author_email="saggaf@nbs.co.id",
+    description="Wavecell REST API Wrapper for Python",
+    license="MIT",
+    long_description="Provide SMS and Mobile Verification integration for Python apps",
+    long_description_content_type="text/markdown",
+    url="https://github.com/saggafarsyad/wavecell-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
