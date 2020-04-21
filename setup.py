c2ed87de8e63b9b54597b201c1b13948ef8fe267
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-04-21 22:32:47 +0700

    fix: added install_requires

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/saggafarsyad/wavecell-python",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'phonenumbers',
+        'requests'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
